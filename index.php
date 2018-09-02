4578b1b9b5354cce78bfc8eeb7c6d0889441419a
Sick <chezsick@bbox.fr>
2018-09-02 17:46:08 +0200

    add race game

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 echo "<h1>Bienvenue Dans RandomLike !</h1>";
-echo "Vous Avez Le Choix De Mes Service ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aleatoire</a><br/><a href='color.php'>Couleur Aleatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aleatoire</a>";
+echo "Vous Avez Le Choix De Mes Service ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aleatoire</a><br/><a href='color.php'>Couleur Aleatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aleatoire</a><br/><a href='race.php?pist=6&len=10'>Course Aleatoire</a>";
---
+++ race.php
@@ -0,0 +1,15 @@
+<?php
+$pist = $_GET["pist"];
+$len = $_GET["len"];
+echo "Votre Resultat De La Course A Ete Generer ::::::<br/>";
+for($i = 0;$i<$pist;$i++)
+{
+$e = $i + 1;
+echo "piste ".$e." :::: ";
+for($l = 0;$l<$len;$l++)
+{
+$g[$i] = $g[$i] + rand(5,15);
+echo $g[$i];echo ", ";
+}echo "<br/>";}
+echo "Le Gagnant A Ete A :::: ".max($g);
+echo "<br/><a href=''>Relancer</a>";
