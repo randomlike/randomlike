b5e3c8076efdd0e7d10d2483db294f983b3adff3
Sick <chezsick@bbox.fr>
2018-09-02 14:08:14 +0200

    add refresh

--- color.php
+++ color.php
@@ -4,3 +4,4 @@ $g = rand(0,255);
 $b = rand(0,255);
 echo "Votre Couleur A Ete Generer :::: rgb(".$r.",".$g.",".$b.")<br/>";
 echo "<svg width='500' height='500'><rect width='500' height='500' style='fill:rgb(".$r.",".$g.",".$b.");stroke-width:1;stroke:rgb(0,0,0)'/></svg>";
+echo "<br/><a href=''>Relancer</a>";
--- number.php
+++ number.php
@@ -2,3 +2,4 @@
 $min = $_GET["min"];
 $max = $_GET["max"];
 echo "Votre Nombre A Ete Generer ::: ".rand($min,$max);
+echo "<br/><a href=''>Relancer</a>";
