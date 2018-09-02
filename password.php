7c1634285627ce85581e8c373799f399e41b02bb
Sick <chezsick@bbox.fr>
2018-09-02 17:20:14 +0200

    ...

--- password.php
+++ password.php
@@ -3,6 +3,6 @@ $len = $_GET["len"];
 echo "Votre Mot De Passe A Ete Generer ::::::: ";
 for($i = 0;$i < $len;$i++)
 {
-echo chr(rand(32,127));
+echo chr(rand(33,126));
 }
 echo "<br/><a href=''>Relancer</a>";
