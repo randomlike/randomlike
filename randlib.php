5c6f4d0058f858ff21c165ccdb352cd127843225
Sick <chezsick@bbox.fr>
2018-09-09 18:37:33 +0200

    modify the randlib for position random source and show counter at home

--- randlib.php
+++ randlib.php
@@ -1,13 +1,19 @@
 <?php
-$randomsource = "/dev/srandom"; // edited if you need
+$randomsource = "sample.bin"; // edited because this is a sample, create a own
+if (!file_exists("counter.txt"))
+{
+file_put_contents("counter.txt",0);
+}
 function srcrand($min,$max)
 {
 global $randomsource;
 $out = 0;
-$raw = file_get_contents($randomsource,null,null,0,8);
+$pos = file_get_contents("counter.txt");
+$raw = file_get_contents($randomsource,null,null,0+$pos,8+$pos);
 for($n = 0;$n < 8;$n++)
 {
 $out =+ (pow(256,$n)*ord($raw[$n]));
 }
+file_put_contents("counter.txt",$pos + 8);
 return intval(($out/pow(256,8))*$max+$min);
 }
