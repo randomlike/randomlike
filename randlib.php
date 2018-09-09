c47e57bd19a530309d181a25f4bf4159399a6344
Chez Sick <chezsick@bbox.fr>
2018-09-09 23:21:40 +0200

    fix the length bit 64 to 24

--- randlib.php
+++ randlib.php
@@ -9,11 +9,11 @@ function srcrand($min,$max)
 global $randomsource;
 $out = 0;
 $pos = file_get_contents("counter.txt");
-$raw = file_get_contents($randomsource,null,null,0+$pos,8+$pos);
-for($n = 0;$n < 8;$n++)
+$raw = file_get_contents($randomsource,null,null,0+$pos,3+$pos);
+for($n = 0;$n < 3;$n++)
 {
 $out =+ (pow(256,$n)*ord($raw[$n]));
 }
-file_put_contents("counter.txt",$pos + 8);
+file_put_contents("counter.txt",$pos + 3);
 return intval((($out/pow(256,8))*($max-$min))+$min);
 }
