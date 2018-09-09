66c6db0062acb9acb2e37f78199f7da3c8c89ee0
Sick <chezsick@bbox.fr>
2018-09-09 19:35:19 +0200

    fix word

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo "<h1>Bienvenue Dans RandomLike !</h1>";
 echo "Vous Avez Le Choix De Mes Service ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aleatoire</a><br/><a href='color.php'>Couleur Aleatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aleatoire</a><br/><a href='race.php?pist=6&len=10'>Course Aleatoire</a>";
 $count = file_get_contents("counter.txt");
-echo "</br>Le Site A Génerer ::::::: " . $count . "Octet";
+echo "</br>Le Site A Génerer ::::::: " . $count . " Octet";
