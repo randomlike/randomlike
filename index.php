50b42857254c3f1d078cc9bf1249e8f4302501c3
Chez Sick <chezsick@bbox.fr>
2018-09-27 23:39:26 +0200

    update of samuel (pull request conflit)
    https://github.com/chezsick/randomlike/pull/1

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $randomsourceinfo = "srandom"; // If You Change The Random Source, Change It, But To Respect The Norm, See normalize_name_randomsourceinfo.csv
 echo "<h1>Bienvenue Dans RandomLike !</h1>";
-echo "Vous Avez Le Choix De Mes Service ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aleatoire</a><br/><a href='color.php'>Couleur Aleatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aleatoire</a><br/><a href='race.php?pist=6&len=10'>Course Aleatoire</a><br/>";
+echo "Vous Avez Le Choix De Mes Services ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aléatoire</a><br/><a href='color.php'>Couleur Aléatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aléatoire</a><br/><a href='race.php?pist=6&len=10'>Course Aléatoire</a><br/>";
 $count = file_get_contents("counter.txt");
-echo "<br/>Le Site A Génerer ::::::: " . $count . " Octet -:- Source Aleatoire :::::: ".$randomsourceinfo;
+echo "<br/>Le Site A Générer ::::::: " . $count . " Octet -:- Source Aléatoire :::::: ".$randomsourceinfo;
