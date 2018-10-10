034ce844fee0d2f37b688601929bd487134d3ff9
Sick <chezsick@bbox.fr>
2018-10-10 15:46:13 +0200

    add bean machine

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $randomsourceinfo = "srandom"; // If You Change The Random Source, Change It, But To Respect The Norm, See normalize_name_randomsourceinfo.csv
 echo "<h1>Bienvenue Dans RandomLike !</h1>";
-echo "Vous Avez Le Choix De Mes Services ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aléatoire</a><br/><a href='color.php'>Couleur Aléatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aléatoire</a><br/><a href='race.php?pist=6&len=10'>Course Aléatoire</a><br/><a href='loto.php?tirage=5&nbball=20'>Tirage Du Loto</a><br/>";
+echo "Vous Avez Le Choix De Mes Services ::::: <br/><a href='number.php?min=0&max=100'>Nombre Aléatoire</a><br/><a href='color.php'>Couleur Aléatoire</a><br/><a href='password.php?len=16'>Mot De Passe Aléatoire</a><br/><a href='race.php?pist=6&len=10'>Course Aléatoire</a><br/><a href='loto.php?tirage=5&nbball=20'>Tirage Du Loto</a><br/><a href='galton.php?len=20'>La Planche De Galton<br/>";
 echo "<br/><a href='syster.php?tp=1&demi=0&frame=1000'>Paramettre D'encodage Nagravision Syster Pour CryptImage</a><br/><a href='videocrypt.php?frame=1000&begin=40&end=215'>Paramettre D'encodage VideoCrypt Pour CryptImage</a><br/>";
 $count = file_get_contents("counter.txt");
 echo "<br/>Le Site A Générer ::::::: " . $count . " Octet -:- Source Aléatoire :::::: ".$randomsourceinfo;
