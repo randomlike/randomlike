3f47438d350210eeabab35dcbec0a6807f36e248
Sick <chezsick@bbox.fr>
2018-10-12 15:08:26 +0200

    fix typo

--- stats.php
+++ stats.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h2>Les Statistique De La Source Aleatoire</h2>";
+echo "<h2>Les Statistique De La Source Aléatoire</h2>";
 require("randlib.php");
 $getcount = file_get_contents("counter.txt");
 $srcrand = file_get_contents($randomsource,null,null,0,$getcount);
@@ -11,6 +11,6 @@ $h[$a] = $h[$a] + 1;
 }
 for ($m = 0;$m < 256;$m++)
 {
-echo "L'octet Numéro ".$m." Apparassais ".$h[$m]." Fois<br/>";
+echo "L'octet Numéro ".$m." Apparaissais ".$h[$m]." Fois<br/>";
 }
-echo "<br/><br/>RAPPELLE ::::: Le Hasard Ne Ce Controle Pas !";
+echo "<br/>RAPPELLE ::::: Le Hasard Ne Ce Controle Pas !";
