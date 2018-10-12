30c7e05457b800556f49f236c05753346df0ed0b
Sick <chezsick@bbox.fr>
2018-10-12 14:44:22 +0200

    add statistic

---
+++ stats.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h2>Les Statistique De La Source Aleatoire</h2>";
+require("randlib.php");
+$getcount = file_get_contents("counter.txt");
+$srcrand = file_get_contents($randomsource,null,null,0,$getcount);
+$h = array_fill(0,256,0);
+for ($n = 0;$n < $getcount;$n++)
+{
+$a = ord($srcrand[$n]);
+$h[$a] = $h[$a] + 1;
+}
+for ($m = 0;$m < 256;$m++)
+{
+echo "L'octet Numéro ".$m." Apparassais ".$h[$m]." Fois<br/>";
+}
+echo "<br/><br/>RAPPELLE ::::: Le Hasard Ne Ce Controle Pas !";
