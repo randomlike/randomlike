733719d9b2afa54537f4dcde0d0bd8facdae21c9
Sick <chezsick@bbox.fr>
2018-10-13 13:52:30 +0200

    updated

--- randlib.php
+++ randlib.php
@@ -9,7 +9,7 @@ function srcrand($min,$max)
 {
 global $raw;
 $out = 0;
-$pos = file_get_contents("counter.txt");
+$pos = intval(file_get_contents("counter.txt"));
 $out = (pow(256,2)*ord($raw[$pos+2]))+(pow(256,1)*ord($raw[$pos+1]))+(pow(256,0)*ord($raw[$pos]));
 file_put_contents("counter.txt",$pos + 3);
 return intval((($out/16777216)*($max-$min))+$min);
@@ -17,7 +17,7 @@ return intval((($out/16777216)*($max-$min))+$min);
 function srcrand8bit()
 {
 global $raw;
-$pos = file_get_contents("counter.txt");
+$pos = intval(file_get_contents("counter.txt"));
 $out = ord($raw[$pos]);
 file_put_contents("counter.txt",$pos + 1);
 return $out;
--- stats.php
+++ stats.php
@@ -1,15 +1,15 @@
 <?php
 echo "<h2>Les Statistique De La Source Aléatoire</h2>";
 require("randlib.php");
-$getcount = file_get_contents("counter.txt");
-$srcrand = file_get_contents($randomsource,null,null,0,$getcount);
+$getcount = intval(file_get_contents("counter.txt"));
+$randmoment = file_get_contents($randomsource,null,null,0,$getcount);
 $h = array_fill(0,256,0);
 $p = array_fill(0,256,0);
 for ($n = 0;$n < $getcount;$n++)
 {
-$a = ord($srcrand[$n]);
+$a = ord($randmoment[$n]);
 $h[$a] = $h[$a] + 1;
-$p[$a] = $n;
+$p[$a] = $n + 1;
 }
 for ($m = 0;$m < 256;$m++)
 {
