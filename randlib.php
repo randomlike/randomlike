df06a6f19c063e6a139ca493ef5971b4dc65aebb
Sick <chezsick@bbox.fr>
2018-10-15 21:58:17 +0200

    add weather generator

--- randlib.php
+++ randlib.php
@@ -22,3 +22,12 @@ $out = ord($raw[$pos]);
 file_put_contents("counter.txt",$pos + 1);
 return $out;
 }
+function srcranduniform($min,$max)
+{
+global $raw;
+$out = 0;
+$pos = intval(file_get_contents("counter.txt"));
+$out = (pow(256,2)*ord($raw[$pos+2]))+(pow(256,1)*ord($raw[$pos+1]))+(pow(256,0)*ord($raw[$pos]));
+file_put_contents("counter.txt",$pos + 3);
+return (($out/16777216)*($max-$min))+$min;
+}
