078f7bd311a6d7bcbde4fe5d8359ec955744e490
Sick <chezsick@bbox.fr>
2018-10-19 03:20:48 +0200

    fix

--- randlib.php
+++ randlib.php
@@ -7,7 +7,7 @@ file_put_contents($randomsourcecount,0);
 $raw = file_get_contents($randomsource);
 function srcrand($min,$max)
 {
-global $raw;
+global $raw,$randomsourcecount;
 $out = 0;
 $pos = intval(file_get_contents($randomsourcecount));
 $out = (pow(256,2)*ord($raw[$pos+2]))+(pow(256,1)*ord($raw[$pos+1]))+(pow(256,0)*ord($raw[$pos]));
@@ -16,7 +16,7 @@ return intval((($out/16777216)*($max-$min))+$min);
 }
 function srcrand8bit()
 {
-global $raw;
+global $raw,$randomsourcecount;
 $pos = intval(file_get_contents($randomsourcecount));
 $out = ord($raw[$pos]);
 file_put_contents($randomsourcecount,$pos + 1);
@@ -24,7 +24,7 @@ return $out;
 }
 function srcranduniform($min,$max)
 {
-global $raw;
+global $raw,$randomsourcecount;
 $out = 0;
 $pos = intval(file_get_contents($randomsourcecount));
 $out = (pow(256,2)*ord($raw[$pos+2]))+(pow(256,1)*ord($raw[$pos+1]))+(pow(256,0)*ord($raw[$pos]));
