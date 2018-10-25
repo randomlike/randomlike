0ee0fe1173307d8ee5878ad9ef0f87723cc55411
Sick <chezsick@bbox.fr>
2018-10-25 10:27:47 +0200

    add bytes

---
+++ bytes.php
@@ -0,0 +1,14 @@
+<?php
+require("randlib.php");
+$len = $_GET["len"];
+header("Content-Type:application/octet-stream");
+$len_limit = 999999; // define the limit
+if ($len > $len_limit)
+{
+echo "C'est Limité à ".$len_limit." Désoler Mais Ne Gaspiller Pas Non Plus";
+exit(1);
+}
+for ($n = 0;$n < $len;$n++)
+{
+echo chr(srcrand8bit());
+}
