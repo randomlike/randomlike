9d0a42fe166f1668f398808479c6f5f1e7c54794
Sick <chezsick@bbox.fr>
2018-12-17 21:50:39 +0100

    special chrismas! :::: generate a name begin code and ?

---
+++ code.php
@@ -0,0 +1,10 @@
+<?php
+echo "<style>@font-face{font-family:code;src:url('code.ttf');}</style>";
+require("randlib.php");
+echo "Ton Nom De La Serie A Ete Générer :::::::::::: ";
+$wordlistpath = "listfr.txt";
+$wordfile = file_get_contents($wordlistpath);
+$wordlistarray = explode("\n",$wordfile);
+$nbword = count($wordlistarray);
+echo "<font face='code' size='7'>code@";
+echo $wordlistarray[srcrand(0,$nbword)]. "</font>";
