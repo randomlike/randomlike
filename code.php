cdfac9194d7189ab6e1b594e70374a02ae4fdd1d
Sick <chezsick@bbox.fr>
2018-12-17 22:20:35 +0100

    replace dict to two syllab by me

--- code.php
+++ code.php
@@ -2,10 +2,10 @@
 echo "<style>@font-face{font-family:code;src:url('code.ttf');}</style>";
 require("randlib.php");
 echo "Ton Nom De La Serie A Ete Générer :::::::::::: ";
-$wordlistpath = "listfr.txt";
-$wordfile = file_get_contents($wordlistpath);
-$wordlistarray = explode("\n",$wordfile);
-$nbword = count($wordlistarray);
+$one = ["ya","lyo","sys","ma","xa","ra","che","ja","ku","ho","ha","geo","kyo","far"];
+$two = ["ko","ki","kia","na","mo","zo","va","ro","cho","tchu"];
+$oc = count($one) - 1;
+$tc = count($two) - 1;
 echo "<font face='code' size='7'>code@";
-echo $wordlistarray[srcrand(0,$nbword)]. "</font>";
+echo $one[srcrand(0,$oc)].$two[srcrand(0,$tc)]. "</font>";
 echo "<br/><a href=''>Relancer</a>";
